816e3917902265b7cd44abbf50aa88472a45e51c
fastee <fastee@126.com>
2023-08-07 10:16:45 +0800

    first commit

---
+++ VIEWMODELS/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace $safeprojectname$.ViewModels
+{
+    partial class MainWindowViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string title = "Title in ViewModel";
+
+
+
+        // Implement command methods here
+        [RelayCommand]
+        private void Open()
+        {
+            // Code for opening a file
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ VIEWS/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using $safeprojectname$.ViewModels;
+
+namespace $safeprojectname$.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    partial class MainWindow : Window
+    {
+        internal static MainWindowViewModel ViewModel { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ExceptionSetting();
+
+            ViewModel = new();
+            DataContext = ViewModel;
+
+        }
+
+        private void ExceptionSetting()
+        {
+            Dispatcher.UnhandledException += (s, e) =>
+            {
+                e.Handled = true;
+                MessageBox.Show(e.Exception.ToString(), "UI.Dispatcher.UnhandledException");
+            };
+
+            TaskScheduler.UnobservedTaskException += (s, e) =>
+            {
+                MessageBox.Show(e.Exception.ToString(), "TaskScheduler.UnobservedTaskException");
+                e.SetObserved();
+            };
+
+            AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+            {
+                StringBuilder sb = new();
+                if (e.ExceptionObject is Exception ex)
+                {
+                    sb.Append(ex.ToString());
+                }
+                else
+                {
+                    sb.Append(e.ExceptionObject);
+                }
+                MessageBox.Show(sb.ToString(), "CurrentDomain.UnhandledException");
+            };
+
+        }
+    }
+}
